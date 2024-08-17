884143b6a5d59e45d2b034d47a437c1c4e229e14
Puddle-UwU <luke.cheney@student.onslow.school.nz>
2024-08-18 01:11:49 +1200

    changes to main

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ def search():
     query = input("query: ").strip().lower()
 
     for item in tree.tree:
+
         item_cleaned = item.strip().lower()
+        related = []
+
         if fuzz.ratio(item_cleaned, query) >= 80 or fuzz.ratio(item_cleaned, f"the {query}") >= 80:
             print(item, tree.tree[item])
+            
+        if fuzz.partial_ratio(item_cleaned, query) >= 70:
+            related.append(item)
+
+    for item in sort_by_similarity(query, related):
+        print(item, tree.tree[item])
+            
+
+
+def sort_by_similarity(target_string, items):
+
+    scored_items = [(item, fuzz.ratio(target_string, item)) for item in items]
+    sorted_items = sorted(scored_items, key=lambda x: x[1], reverse=True)
+    return [item for item, score in sorted_items]
 
 
 search()
